1b7fe44288aa31879f32d87a6e0f9057df11a160
erin <150078746+erinsebast@users.noreply.github.com>
2026-06-02 11:57:40 -0500

    Build careers page and update navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Careers from "./pages/Careers";
@@ -6,6 +7,27 @@ import Home from "./pages/Home";
 function App() {
   const currentPath = window.location.pathname;
   const CurrentPage = currentPath === "/careers" ? Careers : Home;
+
+  useEffect(() => {
+    function scrollToCurrentHash() {
+      if (!window.location.hash) {
+        return;
+      }
+
+      const target = document.querySelector(window.location.hash);
+
+      if (target) {
+        target.scrollIntoView({ block: "start" });
+      }
+    }
+
+    requestAnimationFrame(scrollToCurrentHash);
+    window.addEventListener("hashchange", scrollToCurrentHash);
+
+    return () => {
+      window.removeEventListener("hashchange", scrollToCurrentHash);
+    };
+  }, [currentPath]);
 
   return (
     <div className="min-h-screen bg-blue-50 text-gray-800">
